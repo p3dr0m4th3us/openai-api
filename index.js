da69d55099436e8ff349f5f5c0bb11aa5d291eef
p3dr0m4th3us <46729924+p3dr0m4th3us@users.noreply.github.com>
2022-12-13 19:32:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-// Importe os módulos 'request' e 'fs'
 import pkg from 'request';
 const { post } = pkg;
-// Defina a URL da API do OpenAI
-var openaiApiUrl = 'https://api.openai.com/v1/completions';
+import venom from 'venom-bot'
 
-// Defina sua chave de API do OpenAI
-var openaiApiKey = process.env.OPENAI_API_KEY;
 
-// Defina o modelo que deseja usar
-var model = 'text-davinci-003';
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: true // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
 
-// Defina a solicitação que deseja enviar à API do OpenAI
-var requestData = {
-  // Defina o modelo que deseja usar
-  model: model,
-  // Defina o texto que deseja completar
-  prompt: 'Oi, eu sou o OpenAI. Como posso te ajudar?',
-  // Defina o número de opções de resposta que deseja receber
-  max_tokens: 500
-};
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body[0] === "#" && message.isGroupMsg === false) {
+        console.log(message.body)
+        console.log(String(message.body).slice(1, message.body.length))
+        console.log(message.from)
+        postData(String(message.body).slice(1, message.body.length), message.from, client)
+    }
+  });
+}
 
-// Use o método 'request' para enviar a solicitação à API do OpenAI
-post({url: openaiApiUrl, json: requestData, headers:{"Authorization": "Bearer " + openaiApiKey}}, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    // Imprima a resposta da API do OpenAI no console
-    console.log(body['choices'][0]['text']);
-  }
-});
+
+
+
+function postData(promptText, nmr, client){
+    var openaiApiUrl = 'https://api.openai.com/v1/completions';
+    var openaiApiKey = process.env.OPENAI_API_KEY;
+    var model = 'text-davinci-003';
+    var requestData = {
+        model: model,
+        prompt: promptText,
+        max_tokens: 500,
+        temperature: 1
+      };
+    post({url: openaiApiUrl, json: requestData, headers:{"Authorization": "Bearer " + openaiApiKey}}, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // Imprima a resposta da API do OpenAI no console
+        console.log(body['choices'][0]['text']);
+        console.log(nmr);
+        enviarMensagem(body['choices'][0]['text'], nmr, client);
+    }
+});}
+
+function enviarMensagem(prompt, nmr, client){
+    console.log(prompt)
+    console.log(nmr)
+        client
+            .sendText(nmr, prompt)
+            .then((result) => {
+                console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+                console.error('Error when sending: ', erro); //return object error
+            });
+}
